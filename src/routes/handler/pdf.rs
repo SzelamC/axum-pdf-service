68c0ce0d6e52db19bc595f317538c43a5e68956b
sze_lc <chanszelam7725@gmail.com>
2023-12-09 16:42:31 +0800

    chore: add 1 test case

--- src/routes/handler/pdf.rs
+++ src/routes/handler/pdf.rs
@@ -1,4 +1,5 @@
 use axum::{extract::Multipart, http::StatusCode, response::IntoResponse, Json};
+use poppler::PopplerDocument;
 use serde_json::json;
 
 #[derive(Debug)]
@@ -20,31 +21,56 @@ impl IntoResponse for PDFError {
 }
 
 pub async fn handle_pdf_to_text(mut multipart: Multipart) -> Result<String, PDFError> {
-    use poppler::Document;
-
     let mut data: Vec<u8> = Vec::new();
 
     while let Some(field) = multipart.next_field().await.unwrap() {
         data.append(&mut Vec::from(field.bytes().await.unwrap()));
     }
 
-    let pdf = Document::from_data(&data, None).map_err(|_| {
+    let pdf = PopplerDocument::new_from_data(&mut data, "").map_err(|_| {
         PDFError::IOError(
             StatusCode::BAD_REQUEST,
             "Cannot open pdf file, please try again".to_string(),
         )
     })?;
 
-    let mut content = String::from("");
+    pdf_to_text(&pdf)
+}
 
-    let n_page = pdf.n_pages();
+fn pdf_to_text(pdf_file: &PopplerDocument) -> Result<String, PDFError> {
+    let mut content = String::new();
+    let n_page = pdf_file.get_n_pages();
     for page_idx in 0..n_page {
-        let page = pdf.page(page_idx);
+        let page = pdf_file.get_page(page_idx);
         if let Some(page) = page {
-            if let Some(text) = page.text() {
-                content.push_str(text.as_str());
+            if let Some(text) = page.get_text() {
+                content.push_str(text);
             }
         };
     }
+
     Ok(content)
+}
+
+mod test {
+    use std::path::Path;
+
+    use poppler::PopplerDocument;
+
+    use super::pdf_to_text;
+
+    #[test]
+    fn extract_text_correctly() {
+        let file_path = Path::new("./files/test_1.pdf");
+        let pdf = PopplerDocument::new_from_file(
+            file_path,
+            "",
+        )
+        .unwrap();
+        let content = pdf_to_text(&pdf).unwrap();
+
+        assert!(content.contains("This is a small demonstration .pdf file"));
+        assert!(content.contains("And more text. And more text."));
+        assert!(content.contains("...continued from page 1."));
+    }
 }
